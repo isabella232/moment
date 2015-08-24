7bc3e671fd96e86c805498530c6e32fb7f882b7c
Andrew Kulinich <andrew.kulinich@optimizely.com>
2015-08-24 14:49:35 +0200

    disable locale loading because otherwise webpack expands list of all files in ./locale and includes every file in there into bundle. We don't need loadLocale functionality in a bundle because all the locales we need are preloaded already

--- moment.js
+++ moment.js
@@ -264,13 +264,13 @@
         // TODO: Find a better way to register and load all the locales in Node
         if (!locales[name] && typeof module !== 'undefined' &&
                 module && module.exports) {
-            try {
-                oldLocale = globalLocale._abbr;
-                require('./locale/' + name);
-                // because defineLocale currently also sets the global locale, we
-                // want to undo that for lazy loaded locales
-                locale_locales__getSetGlobalLocale(oldLocale);
-            } catch (e) { }
+            // try {
+            //     oldLocale = globalLocale._abbr;
+            //     require('./locale/' + name);
+            //     // because defineLocale currently also sets the global locale, we
+            //     // want to undo that for lazy loaded locales
+            //     locale_locales__getSetGlobalLocale(oldLocale);
+            // } catch (e) { }
         }
         return locales[name];
     }
